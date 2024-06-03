06a546e406d7392f8c53cd51aadde64e43b1c3ef
henry cullen <h.cullen@pm.me>
2024-06-03 01:09:53 +0100

    Improved readme

--- mail_ip.py
+++ mail_ip.py
@@ -13,11 +13,13 @@ def main():
     subject = "IP Address of the " + device
     public_ip = get_public_ip()
     body = "Hi there "+ sender +",\n\nThe public IP address of the " + device + " is " + public_ip + ".\nThx for using the service. \n\nRegards, \nYour friendly bot"
-    print(password, device, sender, subject, body)
+    # Usefull for debugging
+    # print(password, device, sender, subject, body)
     send_email(subject, body, sender, password)
 
 
 def send_email(subject, body, sender, password):
+    print("Sending email...")
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = sender
@@ -29,12 +31,14 @@ def send_email(subject, body, sender, password):
         print("Message sent!")
 
 def get_public_ip():
+    print("Getting public IP from https://ipinfo.io/json...")
     endpoint = 'https://ipinfo.io/json'
     response = requests.get(endpoint, verify=True)
     if response.status_code != 200:
         return 'Status:', response.status_code, 'Problem with the request. Exiting.'
         exit()
     data = response.json()
+    print("Public IP: ", data['ip'])
     return data['ip']
 
 if __name__ == '__main__':
